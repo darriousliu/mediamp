970c114bc80c70610f241cc28e38fdc416b1ddd2
Him188 <him188@mamoe.net>
2026-09-06 21:44:38 +0900

    fix(test): bundle Skiko runtime for Wasm backend tests

--- mediamp-test/build.gradle.kts
+++ mediamp-test/build.gradle.kts
@@ -21,6 +21,8 @@ kotlin {
     explicitApi()
     wasmJs {
         browser()
+        // Compose UI tests need webpack to bundle the Skiko runtime.
+        binaries.executable()
     }
     androidLibrary {
         namespace = "org.openani.mediamp.test"
